6dd154be3d2616b74f1e878351ba57cc7d2fb84d
Andy Fleming <andy@ulendo.com>
2019-07-29 16:38:14 +0200

    updates

--- buffer.py
+++ buffer.py
@@ -1,9 +1,11 @@
 import arcpy
 import json
+import buffer_procs
+
 
 arcpy.env.overwriteOutput=True
 
-config="mphepo_buffer.json"
+config="mphepo_buffer2.json"
 
 with open(config) as json_file:
     config = json.load(json_file)
@@ -14,52 +16,72 @@ with open(config) as json_file:
 
     buffers=[]
 
-
+  
     for feature in config['features']:
         if 'ignore' in feature:
-            print("Ignoring feature type: " + feature['name'])    
+            print("Ignoring: " + feature['name'])    
         else:
-            print("Buffering feature type: " + feature['name'])
-            #buffer_procs.process(feature)
+            if 'no_preprocess' not in feature:
+                print("Buffering: " + feature['name'])
 
-            #clip input feature to the start_area
-            f_in = config['input_folder'] + "/" + feature['ref'] + ".shp"
-            f_clip = config['input_folder'] + "/" + config['start_area'] + ".shp"
-            f_out = config['output_folder'] + "/" + feature['ref'] + "_clip.shp"
-            arcpy.Clip_analysis(f_in, f_clip, f_out)
+                if feature['input_type'] == 'kmz':
+                    f_in=config['workspace'] + "\\" + config['input_folder'] + "\\" + feature['input']
+                    buffer_procs.import_kmz(f_in,config['temp_workspace'] ,config['workspace'] + "\\" + config['input_folder'],feature['ref'],feature['feature_type'])
 
-            desc = arcpy.Describe(f_out)
-    
-            #reproject if needed
-            if desc.spatialReference.factoryCode != config['coordinate_system']:
-                print("Feature has coordinate system: " + desc.spatialReference.name + ". Projecting to: " +  str(config['coordinate_system']))
+                if feature['input_type'] == 'shp':
+                    f_in=config['workspace'] + "\\" + config['input_folder'] + "\\" + feature['input']    
+                    arcpy.FeatureClassToFeatureClass_conversion(f_in, config['workspace'] + "\\" + config['input_folder'] + "\\", feature['ref'] + ".shp")
+                
+                f_out = config['workspace'] + "\\" + config['input_folder'] + "\\" + feature['ref'] + ".shp" 
+                f_in = f_out
+
+                #repair
+                arcpy.RepairGeometry_management(f_out)
+
+                #reproject if needed
+                desc = arcpy.Describe(f_in)
+                if desc.spatialReference.factoryCode != config['coordinate_system']:
+                    print("Feature has coordinate system: " + desc.spatialReference.name + ". Projecting to: " +  str(config['coordinate_system']))
+                    f_out=config['output_folder'] + "\\" + feature['ref'] + "_proj.shp"
+                    sys= arcpy.SpatialReference(config['coordinate_system'])
+                    arcpy.Project_management(f_in, f_out, sys)
+
+                #clip input feature to the start_area
+                print("Clipping: " + feature['name'])
+                f_in = f_out
+                f_clip = config['input_folder'] + "\\" + config['start_area'] + ".shp"
+                f_out = config['output_folder'] + "\\" + feature['ref'] + "_clip.shp"
+                arcpy.Clip_analysis(f_in, f_clip, f_out)
+        
+
+                #buffer
                 f_in=f_out
-                f_out=config['output_folder'] + "/" + feature['ref'] + "_proj.shp"
-                sys= arcpy.SpatialReference(config['coordinate_system'])
-                arcpy.Project_management(f_in, f_out, sys)
+                f_out = config['output_folder'] + "\\" + feature['ref'] + "_buffer.shp"
+                if feature['buffer_meters']>0:
+                    print("Buffering to: " + f_out)
+                    arcpy.Buffer_analysis(f_in, f_out, feature['buffer_meters'])
+                else:
+                    arcpy.CopyFeatures_management(f_in, f_out)
 
-            #buffer
-            f_in=f_out
-            f_out = config['output_folder'] + "/" + feature['ref'] + "_buffer.shp"
-            print("Buffering to: " + f_out)
-            arcpy.Buffer_analysis(f_in, f_out, feature['buffer_meters'])
-
-            #label
-            f_in=f_out
-            print("Labeling to: " + f_out)
-            arcpy.AddField_management(f_in, "feat_ref", "TEXT", field_length=30)
-            arcpy.CalculateField_management(f_in,"feat_ref",'"' + feature['ref'] + '"')
+                #label
+                f_in=f_out
+                print("Labeling to: " + f_out)
+                arcpy.AddField_management(f_in, "feat_ref", "TEXT", field_length=30)
+                arcpy.CalculateField_management(f_in,"feat_ref",'"' + feature['ref'] + '"')
+    
 
     #merge
+    print('Merging to buffers.shp')
     fs_in=[]
-    f_out = config['output_folder'] + "/buffers.shp"
+    f_out = config['output_folder'] + "\\buffers.shp"
     fm_ref = arcpy.FieldMap()
     fm_fid = arcpy.FieldMap()
     fms = arcpy.FieldMappings()
 
     for feature in config['features']:
-        if 'ignore' not in feature:
-            f_in=config['output_folder'] + "/" + feature['ref'] + "_buffer.shp"
+        if 'ignore' not in feature and 'no_exclusion' not in feature:
+            print("Adding " + feature['name'])
+            f_in=config['output_folder'] + "\\" + feature['ref'] + "_buffer.shp"
             fs_in.append(f_in)            
             fm_ref.addInputField(f_in, "feat_ref") 
             fm_fid.addInputField(f_in, "FID") 
@@ -76,8 +98,16 @@ with open(config) as json_file:
 
     arcpy.Merge_management(fs_in,f_out,fms)
 
+
+    #dissolve buffers
+    print("Dissolving")
+    f_in=f_out
+    f_out=config['output_folder'] + "\\dissolve.shp"
+    arcpy.Dissolve_management(f_in,f_out,"feat_ref")
+
     #clip
-    f_in=config['input_folder'] + "/" + config['start_area'] + ".shp"
+    print("Producing available area")
+    f_in=config['input_folder'] + "\\" + config['start_area'] + ".shp"
     f_erase=f_out
-    f_out=config['output_folder'] + "/available_area.shp"
+    f_out=config['output_folder'] + "\\available_area.shp"
     arcpy.Erase_analysis(f_in, f_erase, f_out)
--- buffer_procs.py
+++ buffer_procs.py
@@ -1,2 +1,35 @@
-def process(input_feature):
-    print(input_feature['name'])
+import arcpy
+import json
+import os
+import shutil
+import glob
+
+def import_kmz(input_feature,temp_location,output_folder,output_name,input_type):
+    delete_kmz_imports(temp_location,output_name)
+    delete_shape(output_folder,output_name)
+
+    print("Importing kmz: " + input_feature + ". Type: " + input_type)
+
+    arcpy.KMLToLayer_conversion(input_feature,temp_location,output_name)
+
+    print("Moving features: " + temp_location + " : " + output_name)
+    f_in=temp_location + "\\" + output_name + ".gdb\\Placemarks\\" + input_type
+    arcpy.FeatureClassToFeatureClass_conversion(f_in, output_folder, output_name)
+
+    
+
+def delete_kmz_imports(folder, base_name):
+    print("Delete kmz: " + folder + "\\" + base_name)
+    
+    if os.path.exists(folder + "\\" + base_name + ".gdb"):
+        shutil.rmtree(folder + "\\" + base_name + ".gdb")
+    if os.path.exists(folder + "\\" + base_name + ".lyr"):
+        os.remove(folder + "\\" + base_name + ".lyr")
+
+def delete_shape(folder,base_name):
+    fileList = glob.glob(folder + "\\" + base_name + ".*")
+    for filePath in fileList:
+        try:
+            os.remove(filePath)
+        except:
+            print("Error while deleting file : ", filePath)        
